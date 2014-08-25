abb28d8ab933cd3bebc833bd1faf4586a7756b5c
zangqilong <zangqilong@gmail.com>
2014-08-25 10:15:37 +0800

    Initial Commit

---
+++ TestMergeVideo/WTLViewController.m
@@ -0,0 +1,29 @@
+//
+//  WTLViewController.m
+//  TestMergeVideo
+//
+//  Created by zang qilong on 14/8/25.
+//  Copyright (c) 2014年 Worktile. All rights reserved.
+//
+
+#import "WTLViewController.h"
+
+@interface WTLViewController ()
+
+@end
+
+@implementation WTLViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
